a8c05eb82b73d103e9c587ed84fe1dc89eb9c49d
nulladmin1 <shrey.deogade@protonmail.com>
2025-05-14 23:47:01 -0400

    Do sum CVLogo

--- CVLogo/CVLogo.java
+++ CVLogo/CVLogo.java
@@ -26,7 +26,24 @@ public class CVLogo extends JPanel {
     // top black line
 
     g.setColor(Color.BLACK);
-    g.drawArc(200, 300, 800, 320, 0, 180);
+
+    int xPos[] = {250, 570, 650, 550};
+    int yPos[] = {300, 280, 330, 300};
+
+    g.fillPolygon(xPos, yPos, xPos.length);
+
+    g.setColor(Color.RED);
+    g.fillOval(560, 345, 40, 30);
+
+    g.setColor(Color.BLACK);
+
+    int xPos2[] = {500, 560, 650, 700, 700, 650};
+    int yPos2[] = {360, 340, 330, 360, 400, 360};
+    g.fillPolygon(xPos2, yPos2, xPos2.length);
+
+    int xPos3[] = {200, 400, 640, 400};
+    int yPos3[] = {400, 550, 380, 500};
+    g.fillPolygon(xPos3, yPos3, xPos3.length);
   }
 }
 ;
